374dd2723b060d813379ba45cb3da618c54489d5
Danil <danil@vast.com>
2020-02-10 22:48:44 -0500

    RED: last_delivery(A)

---
+++ tycoon/test_last_delivery.py
@@ -0,0 +1,15 @@
+import unittest
+
+def lastDelivery(container_schedule):
+    return -1
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(
+            5,
+            lastDelivery("A")
+        )
+
+
+if __name__ == '__main__':
+    unittest.main()
