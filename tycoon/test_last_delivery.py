088494ea6055f02d34c846bbc2c0eea71d69af72
Danil <danil@vast.com>
2020-02-12 23:00:13 -0500

    REFACTOR: arrival_at_A
    Let's make explicit in the code that the delivery time, is a distinct idea from the time of arrival
    because multi leg deliveries have more than one arrival time, and we will need to discriminate.

--- tycoon/test_last_delivery.py
+++ tycoon/test_last_delivery.py
@@ -2,7 +2,9 @@ import unittest
 
 def lastDelivery(container_schedule):
     delivery_times = []
-    delivery_times.append(5)
+    arrival_at_A = 5
+    delivery_time = arrival_at_A
+    delivery_times.append(delivery_time)
     return max(delivery_times)
 
 class MyTestCase(unittest.TestCase):
